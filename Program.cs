ed867891701603d0f0fb12c4231765e0ce751e25
Krzysztof Bogucki <krzysztof.bogucki@aug-e.io>
2022-05-07 08:31:30 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "goose.rmq2.cloudamqp.com",
+    UserName = "lwpfmkvp",
+    VirtualHost = "lwpfmkvp",
+    Password = "ONy0uB8sESyinHD2hRrJ-5Q9hqnUp-4M"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "student",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+    while (true)
+    {
+        string message = $"Wojciech Wołoszka";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: "student",
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine(" [x] Sent {0}", message);
+
+        System.Threading.Thread.Sleep(10000);
+    }
+}
---
+++ Wiadomosc.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Wiadomosc
+{
+    public DateTime Data { get; set; }
+    public string Message { get; set; }
+}
